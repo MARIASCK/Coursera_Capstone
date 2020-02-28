dfcd18a0d353fa8a83206a51b0492b792196284d
MARIASCK <61513409+MARIASCK@users.noreply.github.com>
2020-02-28 17:19:38 +0200

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This Capstone Project Notebook will be used for Capstone Project 
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
